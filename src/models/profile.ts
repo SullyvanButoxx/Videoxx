4ced3c9334feb07f22476801272cd14a127a868f
SullyvanButoxx <butoxxdev@gmail.com>
2018-09-27 16:40:10 +0200

    Add constructors

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -1,5 +1,11 @@
-export interface Profile {
+export class Profile {
     username: string
     firstname: string
     lastname: string
+
+    constructor(username: string, firstname: string, lastname: string) {
+        this.username = username
+        this.firstname = firstname
+        this.lastname = lastname
+    }
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,15 @@
 import { Profile } from "./profile";
 
-export interface User {
+export class User {
     id: string
     email: string
     password: string
     profile: Profile
+
+    constructor(id: string, email: string, password: string, profile: Profile) {
+        this.id = id
+        this.email = email
+        this.password = password
+        this.profile = profile
+    }
 }
