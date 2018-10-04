255e45fda0a82fbe7ebade2b114f7ebb8efa8cae
SullyvanButoxx <butoxxdev@gmail.com>
2018-10-04 17:14:25 +0200

    Add create profile function

--- src/providers/profile/profile.ts
+++ src/providers/profile/profile.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Profile } from '../../models/profile';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FIREBASE_DOCUMENTS } from '../../app/app.firebase.doc';
 
 @Injectable()
 export class ProfileProvider {
 
-  constructor() {
+  constructor(private afStore: AngularFirestore, private afAuth: AngularFireAuth) {
+  }
+
+  create(profile: Profile) {
+    return this.afStore.doc(`${FIREBASE_DOCUMENTS.profile}${this.afAuth.auth.currentUser.uid}`).set(profile.unCustom())
   }
 
 }
